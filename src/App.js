dd925960e0b48b39e0f68bff48c5963bc63bdd3f
neilkrichi <neilkrichi@gmail.com>
2018-05-17 16:08:04 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Pixel Unio Demo</h1>
+          <p className="App-intro">by Neil Krichi</p>
+        </header>
+
+      </div>
+    );
+  }
+}
